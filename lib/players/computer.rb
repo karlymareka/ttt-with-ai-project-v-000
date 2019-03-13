fdaddd51df8a33875e1b3673ff3e6c3b6d3e7149
Karly Mareka <karly.mareka@gmail.com>
2019-03-13 03:02:42 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -62,10 +62,10 @@ module Players
           "5"
         else
           position = rand(1..9)
-          binding.pry 
+          #binding.pry 
           until board.cells[position - 1] == " "
             position = rand(1..9)
-            binding.pry 
+            #binding.pry 
           end
           return position 
         end
